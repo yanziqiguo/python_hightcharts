6caa39af0ae585ca615a9c6089896fa6c577377f
yanbin <342013708@qq.com>
2014-11-22 05:01:06 -0800

    add function current datetime

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from views import hello
+from views import hello, current_datetime
 
 urlpatterns = patterns('',
 	('^hello/$', hello),
+	('^time/$', current_datetime),
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
