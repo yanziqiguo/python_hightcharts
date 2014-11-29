bfbc67457d5f6c84b0f67f5b9b3c6b217717ca68
yanbin <342013708@qq.com>
2014-11-29 03:10:37 -0800

    add more info of Question

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from polls.models import Question
 
 class QuestionAdmin(admin.ModelAdmin):
-	fields = ['pub_date', 'question_text']
-
+	#fields = ['pub_date', 'question_text']
+	fieldsets = [
+		(None, 				 {'fields': ['question_text']}),
+		('Date information', {'fields': ['pub_date'], 'classes':['collapse']}),
+	]
 admin.site.register(Question, QuestionAdmin)
 # Register your models here.
